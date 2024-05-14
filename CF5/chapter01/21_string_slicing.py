734f0f2586678ce822ad545f8f335e3b71fda51e
N. Xaplanteris <xaplanterisnikos@gmail.com>
2024-05-14 20:57:30 +0300

    lesson 2

--- CF5/chapter01/21_string_slicing.py
+++ CF5/chapter01/21_string_slicing.py
@@ -8,3 +8,23 @@ s5 = s[::] # edo exoume step
 
 # negative slicing
 
+s6 = s[:-1] # Coding Factor 
+s7 = s[:-2] # Coding Facto
+
+print(s1)
+print(s2)
+print(s3)
+print(s4)
+print(s5)
+print(s6)
+print(s7)
+
+# small challenge
+# reverse string
+
+r_s = s[::-1]
+print(r_s)
+
+exe_s = s[6::-1]
+
+print(exe_s)
